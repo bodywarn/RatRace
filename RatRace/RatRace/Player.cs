a24dc353994e37d436a670f9cf0410c06d0833fb
Warn <151863271+bodywarn@users.noreply.github.com>
2026-01-09 01:19:55 +0100

    first commit

---
+++ RatRace/RatRace/Player.cs
@@ -0,0 +1,28 @@
+﻿namespace RatRace
+{
+    public class Player
+    {
+        public string Name;
+        private string _password;
+        public string Password { get { return _password; } }
+        public int Money;
+        public List<Bet> Bets;
+
+        public Player(string name, string password, int money)
+        {
+            Name = name;
+            _password = password;
+            Money = money;
+            Bets = new List<Bet>();
+        }
+
+        public bool Login(string name, string password)
+        {
+            if (name == Name && password == Password)
+            {
+                return true;
+            } 
+            return false;
+        } 
+    }
+}
